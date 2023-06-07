01a6fcbeb1ac64437ef909979d897a4ab3d3e4e2
Gerald Bauer <gerald.bauer@gmail.com>
2023-06-07 08:57:11 +0200

    reycle

--- HISTORY.md
+++
@@ -1,4 +0,0 @@
-### 0.0.1 / 2014-08-19
-
-* Everything is new. First release.
-
--- test/helper.rb
+++
@@ -1,9 +0,0 @@
-# encoding: utf-8
-
-require 'minitest/autorun'
-require 'pp'
-
-## our own code
-
-require 'worldlite'
-
